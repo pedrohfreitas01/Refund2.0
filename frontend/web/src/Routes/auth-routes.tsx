4dce3e0220498a3a2a0e48d2708fdecb3dff3b73
pedrohfc <pedrofreitas_ph@hotmail.com>
2025-07-10 18:39:00 -0300

    signup page

--- frontend/web/src/Routes/auth-routes.tsx
+++ frontend/web/src/Routes/auth-routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router";
 import { SignIn } from "../Pages/SignIn";
 import { AuthLayout } from "../components/AuthLayout";
+import { SignUp } from "../Pages/SignUp";
 
 export function AuthRoutes() {
   return (
     <Routes>
       <Route path="/" element={<AuthLayout />} >
         <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Route >
       
     </Routes>
