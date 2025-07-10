2bb4fd88a1778c97d870b6fa2728a211e0c2f80c
pedrohfc <pedrofreitas_ph@hotmail.com>
2025-07-10 18:29:36 -0300

    onSumit state

--- frontend/web/src/Pages/SignIn.tsx
+++ frontend/web/src/Pages/SignIn.tsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+  }
+
   return (
-    <form className="w-full flex flex-col gap-4">
+    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
       <Input
         required
         legend="E-mail"
         type="email"
         placeholder="seu@email.com"
+        onChange={(e) => setEmail(e.target.value)}
       />
 
       <Input
@@ -16,9 +26,19 @@ export function SignIn() {
         legend="Senha"
         type="password"
         placeholder="acb123!"
+        onChange={(e) => setPassword(e.target.value)}
       />
 
-      <Button >Entrar</Button>
+      <Button type="submit" isLoading={isLoading}>
+        Entrar
+      </Button>
+
+      <a
+        href="/signup"
+        className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear"
+      >
+        Criar Conta
+      </a>
     </form>
   );
 }
